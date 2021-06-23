66140994eb0a293ac5026539e51af13b1390dfb3
Lena Namiki <lena.namiki@gmail.com>
2021-06-23 19:21:31 +0900

    Made banner responsive

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,8 +1,10 @@
 import 'bootstrap';
 
-// Internal imports, e.g:
-import { initGlide } from 'plugins/init_glide';
+// navbar menu toggle
+const menuToggle = document.querySelector('.toggle');
+const page = document.querySelector('.page');
 
-document.addEventListener('load', () => {
-  initGlide();
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('.active');
+  page.classList.toggle('.active');
 });
