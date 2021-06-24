84a0eb780e6e92acbd208d0723d8047ad27c6af0
Lena Namiki <lena.namiki@gmail.com>
2021-06-24 09:07:49 -0700

    Added javascript to slide carousel on click

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -8,3 +8,35 @@ menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('active');
   showcase.classList.toggle('active');
 });
+
+// projects carousel
+const carousel = document.querySelector('.carousel');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+carousel.addEventListener('mousedown', (event) => {
+  isDown = true;
+  carousel.classList.add('active');
+  startX = event.pageX - carousel.offsetLeft;
+  scrollLeft = carousel.scrollLeft;
+});
+
+carousel.addEventListener('mouseleave', () => {
+  isDown = false;
+  carousel.classList.remove('active');
+});
+
+carousel.addEventListener('mouseup', () => {
+  isDown = false;
+  carousel.classList.remove('active');
+});
+
+carousel.addEventListener('mousemove', (event) => {
+  if (!isDown) return;
+  event.preventDefault();
+  const x = event.pageX - carousel.offsetLeft;
+  const SCROLL_SPEED = 3;
+  const walk = (x - startX) * SCROLL_SPEED;
+  carousel.scrollLeft = scrollLeft - walk;
+});
